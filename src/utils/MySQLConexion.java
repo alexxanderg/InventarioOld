a2da2dbcc2740a2f4fc7022ab2d7c4d3c3635448
PC_RYZEN <PC_RYZEN@RYZEN5>
2019-10-01 11:50:41 -0500

    A punto de crear Branch Api Happy

--- src/utils/MySQLConexion.java
+++ src/utils/MySQLConexion.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
 public class MySQLConexion {
 	public MySQLConexion(){}
 	
-	private static String URL="jdbc:mysql://localhost:3306/db_inventario?useSSL=false";
+	private static String URL="jdbc:mysql://localhost:3306/db_inventario";
 	private static String USER="root";
 	private static String PASS="Aa123";
 	static{
