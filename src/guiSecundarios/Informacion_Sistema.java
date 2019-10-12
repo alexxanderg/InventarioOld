d0eb2eeb6785378d66c2ce5187d695c86cf36bd7
Jean Carlos Ademir Garcia Palomino <i201623068@cibertec.edu.pe>
2019-10-11 19:41:31 -0500

    Detalles basicos en Informacion_Sistema (Incompleto)

--- src/guiSecundarios/Informacion_Sistema.java
+++ src/guiSecundarios/Informacion_Sistema.java
@@ -13,6 +13,14 @@ public class Informacion_Sistema extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
+	private JLabel lblNewLabel_3;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_5;
+	private JLabel lblNewLabel_6;
+	private JLabel lblReferencia;
+	private JLabel lblCostadoDeNova;
 
 	/**
 	 * Launch the application.
@@ -37,8 +45,40 @@ public class Informacion_Sistema extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
-		lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(10, 11, 46, 14);
+		lblNewLabel = new JLabel("BYTE x BYTE PER\u00DA E.I.R.L");
+		lblNewLabel.setBounds(10, 11, 306, 14);
 		contentPanel.add(lblNewLabel);
+		
+		this.lblNewLabel_1 = new JLabel("RUC:");
+		this.lblNewLabel_1.setBounds(10, 36, 63, 14);
+		contentPanel.add(this.lblNewLabel_1);
+		
+		this.lblNewLabel_2 = new JLabel("Direcci\u00F3n:");
+		this.lblNewLabel_2.setBounds(10, 61, 63, 14);
+		contentPanel.add(this.lblNewLabel_2);
+		
+		this.lblNewLabel_3 = new JLabel("Celular:");
+		this.lblNewLabel_3.setBounds(10, 114, 63, 14);
+		contentPanel.add(this.lblNewLabel_3);
+		
+		this.lblNewLabel_4 = new JLabel("20604635447");
+		this.lblNewLabel_4.setBounds(83, 36, 160, 14);
+		contentPanel.add(this.lblNewLabel_4);
+		
+		this.lblNewLabel_5 = new JLabel("Calle Octavio Mu\u00F1oz Najar 20B 213 Interior 207");
+		this.lblNewLabel_5.setBounds(83, 61, 233, 14);
+		contentPanel.add(this.lblNewLabel_5);
+		
+		this.lblNewLabel_6 = new JLabel("986865523");
+		this.lblNewLabel_6.setBounds(83, 114, 95, 14);
+		contentPanel.add(this.lblNewLabel_6);
+		
+		this.lblReferencia = new JLabel("Referencia:");
+		this.lblReferencia.setBounds(10, 86, 62, 14);
+		contentPanel.add(this.lblReferencia);
+		
+		this.lblCostadoDeNova = new JLabel("Costado de Nova Center");
+		this.lblCostadoDeNova.setBounds(77, 86, 150, 14);
+		contentPanel.add(this.lblCostadoDeNova);
 	}
 }
